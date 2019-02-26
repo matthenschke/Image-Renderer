995d3921a4670d3241ae4ff52693ba44b10c274a
Matthew Henschke <matthewhenschke98@gmail.com>
2019-02-26 08:52:51 -0500

    Updates

---
+++ precache-manifest.a57b952b78e08938ceac3b0833c7b5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7a4f42aa407e07d4b320",
+    "url": "/static/js/main.7a4f42aa.chunk.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "7a4f42aa407e07d4b320",
+    "url": "/static/css/main.45f1ff3a.chunk.css"
+  },
+  {
+    "revision": "e7aeebb89e247750eb584b63d890a945",
+    "url": "/index.html"
+  }
+];
